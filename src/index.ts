8e0ceba78275e051f3478693fe8ff29a85c80bc1
Labhansh Agrawal <labhansh.agrawal@gmail.com>
2021-02-16 21:46:40 +0530

    Add ILink options and JSDoc comments

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,56 @@
-import type {ILinkProvider, ILink, Terminal, IViewportRange, IBufferCellPosition} from 'xterm';
+import type {IBufferCellPosition, ILink, ILinkDecorations, ILinkProvider, Terminal} from 'xterm';
 
 interface ILinkProviderOptions {
-  hover?(event: MouseEvent, text: string, location: IViewportRange): void;
-  leave?(event: MouseEvent, text: string): void;
+  hover?: (event: MouseEvent, text: string) => void;
+  leave?: (event: MouseEvent, text: string) => void;
+  dispose?: () => void;
+  decorations?: ILinkDecorations;
 }
 
 export class LinkProvider implements ILinkProvider {
+  /**
+   * Create a Link Provider for xterm.js
+   * @param _terminal The terminal instance
+   * @param _regex The regular expression to use for matching
+   * @param _handler Callback for when link is clicked
+   * @param _options Further hooks, eg. hover, leave and decorations
+   */
   constructor(
     private readonly _terminal: Terminal,
     private readonly _regex: RegExp,
-    private readonly _handler: (event: MouseEvent, uri: string) => void,
+    private readonly _handler: ILink['activate'],
     private readonly _options: ILinkProviderOptions = {}
   ) {}
 
   public provideLinks(y: number, callback: (links: ILink[] | undefined) => void): void {
-    const links = computeLink(y, this._regex, this._terminal, this._handler);
-    callback(this._addCallbacks(links));
-  }
-
-  private _addCallbacks(links: ILink[]): ILink[] {
-    return links.map((link) => {
-      link.leave = this._options.leave;
-      link.hover = (event: MouseEvent, uri: string): void => {
-        if (this._options.hover) {
-          const {range} = link;
-          this._options.hover(event, uri, range);
-        }
-      };
-      return link;
-    });
+    const links = computeLink(y, this._regex, this._terminal).map(
+      (_link): ILink => ({
+        range: _link.range,
+        text: _link.text,
+        activate: this._handler,
+        decorations: this._options.decorations,
+        dispose: this._options.dispose,
+        hover: this._options.hover,
+        leave: this._options.leave
+      })
+    );
+    callback(links);
   }
 }
-
-export const computeLink = (
-  y: number,
-  regex: RegExp,
-  terminal: Terminal,
-  handler: (event: MouseEvent, uri: string) => void
-): ILink[] => {
+/**
+ * Find link range and text for the given line and regex
+ * @param y The line number to process
+ * @param regex The regular expression to use for matching
+ * @param terminal The terminal instance
+ */
+export const computeLink = (y: number, regex: RegExp, terminal: Terminal) => {
   const rex = new RegExp(regex.source, (regex.flags || '') + 'g');
 
   const [line, startLineIndex] = translateBufferLineToStringWithWrap(y - 1, terminal);
 
   let match;
   let stringIndex = -1;
-  const result: ILink[] = [];
+  const result: Pick<ILink, 'range' | 'text'>[] = [];
 
   while ((match = rex.exec(line)) !== null) {
     const text = match[1];
@@ -71,18 +77,12 @@ export const computeLink = (
       end: stringIndexToBufferPosition(terminal, startLineIndex, stringIndex + text.length - 1)
     };
 
-    result.push({range, text, activate: handler});
+    result.push({range, text});
   }
 
   return result;
 };
 
-/**
- * Gets the entire line for the buffer line
- * @param line The line being translated.
- * @param trimRight Whether to trim whitespace to the right.
- * @param terminal The terminal
- */
 const translateBufferLineToStringWithWrap = (
   lineIndex: number,
   terminal: Terminal
